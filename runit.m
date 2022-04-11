446af0bcf08de1c0769c3c96b0d9968ad8b568bc
Tahvonen <osstahv@ad.helsinki.fi>
2022-04-11 14:32:13 +0300

    modded files

--- runit.m
+++ runit.m
@@ -1,5 +1,10 @@
 %basicgrowthmodel(1.5)
 R=realbusinesscycles()
-plot(R.Simulated.k)
-plot(R.Simulated.i_s)
-plot(R.Simulated.y)
+%plot(R.Simulated.k)
+%plot(R.Simulated.i_s)
+%plot(R.Simulated.y)
+
+res = ["Variable", "Mean", "SD";
+    "Capital",mean(R.Simulated.k),std(R.Simulated.k);
+    "Investment",mean(R.Simulated.inv),std(R.Simulated.inv);
+    "Consumption",mean(R.Simulated.c),std(R.Simulated.c)]
